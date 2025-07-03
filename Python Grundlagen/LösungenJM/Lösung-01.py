317eb1ca0a64c6c913df1461eda70dc434b1fde7
Qba <juanclaudio.bedla@freenet.de>
2025-07-03 11:57:48 +0200

    Redundanz (Folder: Python Grundlagen) entfernt

--- Python Grundlagen/LösungenJM/Lösung-01.py
+++
@@ -1,1 +0,0 @@
-print("\n\n  Hurra - es klappt")
--- Python Grundlagen/LösungenJM/Lösung-03.py
+++
@@ -1,7 +0,0 @@
-def maximum(z1, z2):
-    if z1 >= z2:
-        return z1
-    else:
-        return z2
-    
-print(maximum(int(input("Zahl1: ")), int(input("Zahl2: "))))
